1ea029280a784f79146afb0350b6090e829817d7
Nihil 383 <engyayman@aucegypt.edu>
2026-03-29 18:09:20 +0200

    Services is now an object

--- include/main.cpp
+++ include/main.cpp
@@ -1,13 +1,43 @@
 #include "LinkedList.h"
 #include <iostream>
 #include <string>
+using namespace std;
+
+//global counter
+static long long serviceCount = 0;
+
+//function to generate the next unique service id
+long long newId() {
+    return ++serviceCount;
+}
+
+class Service {
+private:
+    long long id;       // used as the index in graph
+    string name;        // normal name
+    bool affected;      //will be true when affected
+
+public:
+    //Constructors
+    Service() : id(newId()), name("NA"), affected(false) {}
+    Service(string name) : id(newId()), name(name), affected(false) {}
+
+    //getters
+    long long getId() const {return id;}
+    string getName() const {return name;}
+    bool getAffected() const {return affected;}
+
+    //setters
+    void setName(string name) {this->name = name;}
+    void setAffected(bool affected) {this->affected = affected;}
+};
 
 int main() {
-    LinkedList<std::string> services;
+    LinkedList<Service> services;
     
-    services.push_back("Database");
-    services.push_back("API");
-    services.push_front("Auth");
+    /*services.push_back(Service("Database"));
+    services.push_back(Service("API"));
+    services.push_front(Service("Auth"));
     
     std::cout << "Services: ";
     services.print();
@@ -16,7 +46,7 @@ int main() {
     services.remove("API");
     std::cout << "After removal: ";
     services.print();
-    std::cout << std::endl;
+    std::cout << std::endl;*/
     
     return 0;
 }
