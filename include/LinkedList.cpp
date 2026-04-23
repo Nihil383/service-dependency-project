8918a917f172ac807e9a8acd34bd12de421497d5
Asmaa <asmaasaad@aucegypt.edu>
2026-04-23 12:54:45 +0200

    Update LinkedList.cpp

--- include/LinkedList.cpp
+++ include/LinkedList.cpp
@@ -1,106 +1,45 @@
 #include "LinkedList.h"
-#include <iostream>
 
-template<typename T>
-LinkedList<T>::LinkedList() : head(nullptr), tail(nullptr), listSize(0) {}
+LinkedList::LinkedList() : head(nullptr), tail(nullptr), listSize(0) {}
 
-template<typename T>
-LinkedList<T>::LinkedList(const LinkedList& other) : head(nullptr), tail(nullptr), listSize(0) {
-    Node* current = other.head;
-    while (current) {
-        push_back(current->data);
-        current = current->next;
-    }
-}
-
-template<typename T>
-LinkedList<T>::~LinkedList() {
+LinkedList::~LinkedList() {
     clear();
 }
 
-template<typename T>
-LinkedList<T>& LinkedList<T>::operator=(const LinkedList& other) {
-    if (this != &other) {
-        clear();
-        Node* current = other.head;
-        while (current) {
-            push_back(current->data);
-            current = current->next;
-        }
-    }
-    return *this;
-}
-
-template<typename T>
-void LinkedList<T>::push_front(const T& value) {
+// Add to end (enqueue)
+void LinkedList::push_back(const std::string& value) {
     Node* newNode = new Node(value);
-    newNode->next = head;
-*/if (!head) {
-        tail = newNode;
-    }/*
-    head = newNode;
-    listSize++;
-}
-
-template<typename T>
-void LinkedList<T>::push_back(const T& value) {
-    Node* newNode = new Node(value);
+    
     if (!head) {
         head = newNode;
+        tail = newNode;
     } else {
-        Node* current = head;
-        while (current->next) current = current->next;
-        current->next = newNode;
-/*tail->next = newNode;
-        tail = newNode;*/
+        tail->next = newNode;
+        tail = newNode;
     }
     listSize++;
 }
 
-template<typename T>
-bool LinkedList<T>::pop_front() {
+//  Remove from front (dequeue)
+bool LinkedList::pop_front() {
     if (!head) return false;
+    
     Node* temp = head;
     head = head->next;
-*/if (head == nullptr) {
+    
+    if (!head) {
         tail = nullptr;
-    }/*
+    }
+    
     delete temp;
     listSize--;
     return true;
 }
 
-//needs update for tail
-template<typename T>
-bool LinkedList<T>::remove(const T& value) {
-    if (!head) return false;
-    
-    if (head->data == value) {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-        listSize--;
-        return true;
-    }
-    
-    Node* current = head;
-    while (current->next && current->next->data != value) {
-        current = current->next;
-    }
-    
-    if (current->next) {
-        Node* temp = current->next;
-        current->next = temp->next;
-        delete temp;
-        listSize--;
-        return true;
-    }
-    
-    return false;
+bool LinkedList::empty() const {
+    return head == nullptr;
 }
-
-template<typename T>
-bool LinkedList<T>::contains(const T& value) const {
+bool LinkedList::contains(const std::string& value) const {
     Node* current = head;
     while (current) {
         if (current->data == value) return true;
@@ -109,29 +48,59 @@ bool LinkedList<T>::contains(const T& value) const {
     return false;
 }
 
-template<typename T>
-size_t LinkedList<T>::size() const {
-    return listSize;
+// Remove service from list
+void LinkedList::remove(const std::string& value) {
+    if (!head) return;
+    
+    // Remove from head
+    if (head->data == value) {
+        Node* temp = head;
+        head = head->next;
+        
+        if (!head) {
+            tail = nullptr;
+        }
+        
+        delete temp;
+        listSize--;
+        return;
+    }
+    
+    // Remove from middle or end
+    Node* current = head;
+    while (current->next && current->next->data != value) {
+        current = current->next;
+    }
+    
+    if (current->next) {
+        Node* temp = current->next;
+        current->next = temp->next;
+        
+        if (!current->next) {
+            tail = current;
+        }
+        
+        delete temp;
+        listSize--;
+    }
 }
 
-template<typename T>
-bool LinkedList<T>::empty() const {
-    return head == nullptr;
-}
-
-template<typename T>
-void LinkedList<T>::clear() {
+// Reset all services
+void LinkedList::clear() {
     while (head) {
         Node* temp = head;
         head = head->next;
         delete temp;
     }
-/*tail = nullptr*/
+    tail = nullptr;
     listSize = 0;
 }
 
-template<typename T>
-void LinkedList<T>::print() const {
+int LinkedList::size() const {
+    return listSize;
+}
+
+void LinkedList::print() const {
     Node* current = head;
     std::cout << "[";
     while (current) {
