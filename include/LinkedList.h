d9cd99c5ecee214d3f0bb8120d162ca127eab506
Asmaa <asmaasaad@aucegypt.edu>
2026-04-23 12:58:35 +0200

    Update LinkedList.h

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -1,48 +1,38 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-template<typename T>
+#include <iostream>
+#include <string>
+
 class LinkedList {
 private:
     struct Node {
-        T data;
+        std::string data;
         Node* next;
-        Node(const T& value) : data(value), next(nullptr) {}
+        Node(const std::string& value) : data(value), next(nullptr) {}
     };
     
     Node* head;
     Node* tail;
-    size_t listSize;
+    int listSize;
     
 public:
     LinkedList();
-    LinkedList(const LinkedList& other);
     ~LinkedList();
-    LinkedList& operator=(const LinkedList& other);
     
-    void push_front(const T& value);
-    void push_back(const T& value);
+    // For Queue operations 
+    void push_back(const std::string& value);
     bool pop_front();
-    bool remove(const T& value);
-    bool contains(const T& value) const;
-    size_t size() const;
     bool empty() const;
+    
+    // For Status tracking
+    bool contains(const std::string& value) const;
+    void remove(const std::string& value);
     void clear();
+    
+    // Utility
+    int size() const;
     void print() const;
-    
-    class Iterator {
-    private:
-        Node* current;
-    public:
-        Iterator(Node* node) : current(node) {}
-        T& operator*() { return current->data; }
-        Iterator& operator++() { if (current) current = current->next; return *this; }
-        bool operator!=(const Iterator& other) const { return current != other.current; }
-    };
-    
-    Iterator begin() { return Iterator(head); }
-    Iterator end() { return Iterator(nullptr); }
 };
 
-#include "LinkedList.cpp"
 #endif
